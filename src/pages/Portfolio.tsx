c4854274306199603f5661eb06ed28005e713444
Lucas Maranho <lksribeiro2014@gmail.com>
2024-04-18 13:28:30 -0300

    deploy

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -56,6 +56,16 @@ const projetos: Projeto[] = [
     data: "01/02/2024",
     imagem: "/camisetas-mix.jpg"
   },
+  {
+    nome: "Página de Categoria com Filtros",
+    descricao: "Simulando o desenvolvimento de filtro e página de categoria de um e-commerce.",
+    tags: ['GulpS', 'Sass/scss', 'Typescript', 'Webpack','HTML', 'Javascript', 'Vanilla JavaScript', 'API'],
+    github: "https://github.com/Front-LucasRibeiro/desenvolvedor-cadastra",
+    private: false,
+    linkProjeto: "",
+    data: "22/03/2024",
+    imagem: ""
+  },
 ];
 
 const Portfolio: React.FC  = () => {
